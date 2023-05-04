6b7ea6235d6d3ef3e0241afc5fd2d962486108ca
Daniel Santana <danielgsantana@gmail.com>
2023-05-04 16:58:55 +0100

    Missing opacity method. Macro to ease event creation on leptos side.

--- src/components/map_events.rs
+++ src/components/map_events.rs
@@ -1,10 +1,10 @@
+use leaflet::{ErrorEvent, Event, LocationEvent, PopupEvent};
 use std::cell::RefCell;
 use std::rc::Rc;
-use leaflet::{ErrorEvent, Event, LocationEvent, PopupEvent};
 
 #[derive(Clone, Default)]
 pub struct MapEvents {
-    inner: Rc<RefCell<InnerMapEvents>>
+    inner: Rc<RefCell<InnerMapEvents>>,
 }
 
 #[derive(Default)]
@@ -16,73 +16,37 @@ struct InnerMapEvents {
     resize: Option<Box<dyn Fn(Event)>>,
     popup_open: Option<Box<dyn Fn(PopupEvent)>>,
     popup_close: Option<Box<dyn Fn(PopupEvent)>>,
+    zoom: Option<Box<dyn Fn(Event)>>,
+}
+
+macro_rules! leaflet_event {
+    ($with:ident, $take:ident, $e:ident, $t:ty) => {
+        impl MapEvents {
+            /// Set ups event $n
+            pub fn $with(self, callback: impl Fn($t) + 'static) -> Self {
+                self.inner.borrow_mut().$e = Some(Box::new(callback));
+                self
+            }
+
+            /// Takes the callback for event $n from the struture
+            pub fn $take(&self) -> Option<Box<dyn Fn($t)>> {
+                self.inner.borrow_mut().$e.take()
+            }
+        }
+    };
 }
 
 impl MapEvents {
     pub fn new() -> Self {
         Self::default()
     }
-
-    pub fn with_location_found(self, callback: impl Fn(LocationEvent) + 'static) -> Self {
-        self.inner.borrow_mut().location_found = Some(Box::new(callback));
-        self
-    }
-
-    pub fn with_location_error(self, callback: impl Fn(ErrorEvent) + 'static) -> Self {
-        self.inner.borrow_mut().location_error = Some(Box::new(callback));
-        self
-    }
-
-    pub fn with_popup_open(self, callback: impl Fn(PopupEvent) + 'static) -> Self {
-        self.inner.borrow_mut().popup_open = Some(Box::new(callback));
-        self
-    }
-
-    pub fn with_popup_close(self, callback: impl Fn(PopupEvent) + 'static) -> Self {
-        self.inner.borrow_mut().popup_close = Some(Box::new(callback));
-        self
-    }
-
-    pub fn with_load(self, callback: impl Fn(Event) + 'static) -> Self {
-        self.inner.borrow_mut().load = Some(Box::new(callback));
-        self
-    }
-
-    pub fn with_unload(self, callback: impl Fn(Event) + 'static) -> Self {
-        self.inner.borrow_mut().unload = Some(Box::new(callback));
-        self
-    }
-
-    pub fn with_resize(self, callback: impl Fn(Event) + 'static) -> Self {
-        self.inner.borrow_mut().resize = Some(Box::new(callback));
-        self
-    }
-
-    pub fn take_location_found(self) -> Option<Box<dyn Fn(LocationEvent)>> {
-        self.inner.borrow_mut().location_found.take()
-    }
-
-    pub fn take_location_error(self) -> Option<Box<dyn Fn(ErrorEvent)>> {
-        self.inner.borrow_mut().location_error.take()
-    }
-
-    pub fn take_popup_open(self) -> Option<Box<dyn Fn(PopupEvent)>> {
-        self.inner.borrow_mut().popup_open.take()
-    }
-
-    pub fn take_popup_close(self) -> Option<Box<dyn Fn(PopupEvent)>> {
-        self.inner.borrow_mut().popup_close.take()
-    }
-
-    pub fn take_load(self) -> Option<Box<dyn Fn(Event)>> {
-        self.inner.borrow_mut().load.take()
-    }
-
-    pub fn take_unload(self) -> Option<Box<dyn Fn(Event)>> {
-        self.inner.borrow_mut().unload.take()
-    }
-
-    pub fn take_resize(self) -> Option<Box<dyn Fn(Event)>> {
-        self.inner.borrow_mut().resize.take()
-    }
 }
+
+leaflet_event!(with_zoom, take_zoom, zoom, Event);
+leaflet_event!(with_location_found, take_location_found, location_found, LocationEvent);
+leaflet_event!(with_location_error, take_location_error, location_error, ErrorEvent);
+leaflet_event!(with_popup_open, take_popup_open, popup_open, PopupEvent);
+leaflet_event!(with_popup_close, take_popup_close, popup_close, PopupEvent);
+leaflet_event!(with_load, take_load, load, Event);
+leaflet_event!(with_unload, take_unload, unload, Event);
+leaflet_event!(with_resize, take_resize, resize, Event);
