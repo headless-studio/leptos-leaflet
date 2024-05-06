1d97009f9233fbca69e71259988f426a80a08289
Lewin Probst <info@emirror.de>
2024-05-06 16:13:56 +0200

    feat: Added parameter bring_to_front and bring_to_back to tile layer and tile layer wms component

--- leptos-leaflet/src/components/tile_layer.rs
+++ leptos-leaflet/src/components/tile_layer.rs
@@ -1,11 +1,14 @@
 use leptos::*;
 
 use crate::components::context::LeafletMapContext;
+use log::warn;
 
 #[component(transparent)]
 pub fn TileLayer(
     #[prop(into)] url: String,
     #[prop(into, optional)] attribution: String,
+    #[prop(optional)] bring_to_front: bool,
+    #[prop(optional)] bring_to_back: bool,
 ) -> impl IntoView {
     let map_context = use_context::<LeafletMapContext>().expect("map context not found");
 
@@ -17,6 +20,14 @@ pub fn TileLayer(
             }
             let map_layer = leaflet::TileLayer::new_options(&url, &options);
             map_layer.add_to(&map);
+
+            match (bring_to_front, bring_to_back) {
+                (true, true) => warn!("The parameters are set to bring the layer to front and back at the same time. Ignoring these parameters..."),
+                (true, false) => {map_layer.bring_to_front();}
+                (false, true) => {map_layer.bring_to_back();}
+                (false, false) => (),
+            }
+
             on_cleanup(move || {
                 map_layer.remove();
             });
--- leptos-leaflet/src/components/tile_layer_wms.rs
+++ leptos-leaflet/src/components/tile_layer_wms.rs
@@ -4,12 +4,15 @@ use crate::components::context::LeafletMapContext;
 use crate::components::context::TileLayerWmsContext;
 use crate::MapEvents;
 use leaflet::{Map, TileLayerWms as LeafletTileLayerWms, TileLayerWmsOptions};
+use log::warn;
 
 #[component(transparent)]
 pub fn TileLayerWms(
     #[prop(into)] url: String,
     options: TileLayerWmsOptions,
     #[prop(optional)] children: Option<Children>,
+    #[prop(optional)] bring_to_front: bool,
+    #[prop(optional)] bring_to_back: bool,
 ) -> impl IntoView {
     let map_context = use_context::<LeafletMapContext>().expect("map context not found");
     let wms_context = TileLayerWmsContext::new();
@@ -20,6 +23,14 @@ pub fn TileLayerWms(
             let map_layer = leaflet::TileLayerWms::new_options(&url, &options);
             map_layer.add_to(&map);
             wms_context.set_wms(&map_layer);
+
+            match (bring_to_front, bring_to_back) {
+                (true, true) => warn!("The parameters are set to bring the layer to front and back at the same time. Ignoring these parameters..."),
+                (true, false) => {map_layer.bring_to_front();}
+                (false, true) => {map_layer.bring_to_back();}
+                (false, false) => (),
+            }
+
             on_cleanup(move || {
                 map_layer.remove();
             });
