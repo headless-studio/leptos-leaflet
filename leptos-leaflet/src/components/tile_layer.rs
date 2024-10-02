e7c8fb9b880f11b5605fe271fa2f3617ebaaa571
Oto Petřík <oto.petrik@gmail.com>
2024-10-02 08:27:55 +0200

    feat: Add min_zoom and max_zoom to TileLayer

--- leptos-leaflet/src/components/tile_layer.rs
+++ leptos-leaflet/src/components/tile_layer.rs
@@ -9,6 +9,8 @@ pub fn TileLayer(
     #[prop(into, optional)] attribution: String,
     #[prop(optional)] bring_to_front: bool,
     #[prop(optional)] bring_to_back: bool,
+    #[prop(default=0.0)] min_zoom: f64,
+    #[prop(default=18.0)] max_zoom: f64,
 ) -> impl IntoView {
     let map_context = use_context::<LeafletMapContext>().expect("map context not found");
 
@@ -18,6 +20,8 @@ pub fn TileLayer(
             if !attribution.is_empty() {
                 options.set_attribution(attribution.to_string());
             }
+            options.set_min_zoom(min_zoom);
+            options.set_max_zoom(max_zoom);
             let map_layer = leaflet::TileLayer::new_options(&url, &options);
             map_layer.add_to(&map);
 
